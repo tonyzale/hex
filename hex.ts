88af86ee6eb391a64e8040fc4be7783b7c670d53
Tony Zale <tonyzale@gmail.com>
2016-10-24 21:27:26 -0500

    lint fixes

--- hex.ts
+++ hex.ts
@@ -9,26 +9,26 @@ namespace HexGame {
     constructor(public x: number, public y: number, public inner_r: number, public id: number) {
       // outer r is a side-length of the triangles forming the hex
       this.outer_r = inner_r / Math.sqrt(3) * 2;
-      var top = y - inner_r;
-      var bottom = y + inner_r;
-      var left = x - this.outer_r;
-      var right = x + this.outer_r;
-      var inner_left = x - (this.outer_r * 0.5);
-      var inner_right = x + (this.outer_r * 0.5);
+      const top = y - inner_r;
+      const bottom = y + inner_r;
+      const left = x - this.outer_r;
+      const right = x + this.outer_r;
+      const inner_left = x - (this.outer_r * 0.5);
+      const inner_right = x + (this.outer_r * 0.5);
       this.corners[0] = [left, y];
       this.corners[1] = [inner_left, top];
       this.corners[2] = [inner_right, top];
       this.corners[3] = [right, y];
       this.corners[4] = [inner_right, bottom];
       this.corners[5] = [inner_left, bottom];
-      for (var c of this.corners) {
+      for (const c of this.corners) {
         this.poly_string += `${c[0]},${c[1]} `;
       }
       this.poly_string += `${this.corners[0][0]},${this.corners[0][1]}`;
     }
     class_name(): string {
-      if (this.owner != undefined) return this.owner.class_name;
-      if (this.temp_class != "") return this.temp_class;
+      if (this.owner !== undefined) return this.owner.class_name;
+      if (this.temp_class !== "") return this.temp_class;
       return "unowned";
     }
   }
@@ -43,12 +43,12 @@ namespace HexGame {
       let hex_count = 0;
       for (var i = 0; i < side_size; ++i) {
         this.hex_board[i] = [];
-        for (var j = 0; j < side_size; ++j) {
+        for (let j = 0; j < side_size; ++j) {
           this.hex_board[i][j] = new Hex(x, y, hex_rad, hex_count);
           this.hexes[hex_count++] = this.hex_board[i][j];
           y += hex_rad * 2;;
         }
-        if (i % 2 == 0) {
+        if (i % 2 === 0) {
           y = hex_rad * 3;
         } else {
           y = hex_rad * 2;
@@ -65,10 +65,9 @@ namespace HexGame {
       let adjacents: Hex[] = [];
       let i = Math.floor(hex.id / this.side_size);
       let j = hex.id % this.side_size;
-      if (this.hex_board[i][j] != hex) {
+      if (this.hex_board[i][j] !== hex) {
         throw new Error("Unexpected Hex at hex_board[" + i + "][" + j + "]");
       }
-      
       // above
       if (this.valid_loc(i, j - 1)) {
         adjacents.push(this.hex_board[i][j - 1]);
@@ -78,7 +77,7 @@ namespace HexGame {
         adjacents.push(this.hex_board[i][j + 1]);
       }
       let up = 0;
-      if ((i % 2) == 0) {
+      if ((i % 2) === 0) {
         up = -1;
       }
       let down = up + 1;
@@ -131,7 +130,7 @@ namespace HexGame {
       return false;
     }
 
-    can_reach(open: Hex[], destination: Hex[], explored: Hex[], adjacent_filter: (hex:Hex)=>boolean): boolean {
+    can_reach(open: Hex[], destination: Hex[], explored: Hex[], adjacent_filter: (hex: Hex) => boolean): boolean {
       while (open.length > 0) {
         let current = open.pop();
         if (this.hexlist_contains(destination, current)) {
@@ -160,10 +159,10 @@ namespace HexGame {
         destination_p2.push(this.board.hex_board[i][this.board.side_size - 1]);
       }
       let game = this;
-      if (this.can_reach(start_p1, destination_p1, start_p1.slice(0), function(hex:Hex){ return hex.owner === game.players[0];})) {
+      if (this.can_reach(start_p1, destination_p1, start_p1.slice(0), function(hex: Hex){ return hex.owner === game.players[0]; })) {
         return this.players[0];
       }
-      if (this.can_reach(start_p2, destination_p2, start_p2.slice(0), function(hex:Hex){ return hex.owner === game.players[1];})) {
+      if (this.can_reach(start_p2, destination_p2, start_p2.slice(0), function(hex: Hex){ return hex.owner === game.players[1]; })) {
         return this.players[1];
       }
       return undefined;
@@ -182,12 +181,12 @@ namespace HexGame {
     }
 
     hex_leave(id: number) {
-      if (this.board.hexes[id].temp_class == "hover") {
+      if (this.board.hexes[id].temp_class === "hover") {
         this.board.hexes[id].temp_class = "";
       }
       let adjacents = this.board.adjacent_hexes(this.board.hexes[id]);
       adjacents.forEach(function(h) {
-        if (h.temp_class == "adjacent") {
+        if (h.temp_class === "adjacent") {
           h.temp_class = "";
         }
       });
